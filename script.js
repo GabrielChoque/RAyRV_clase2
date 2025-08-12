1bd66d8046bfe010bcbf27722a889953a7b44986
GabrielChoque <lgabrielalejandro6480@gmail.com>
2025-08-11 20:20:32 -0400

    RA Y RV con Ar.js de un cubo

---
+++ script.js
@@ -0,0 +1,24 @@
+// varibles
+let counter = 0;
+const colors = ['#FF3333', '#33FF33', '#3333FF', '#FFFF33', '#FF33FF']
+// DOM
+document.addEventListener('DOMContentLoaded', () => {
+    const arBox = document.getElementById('arBox');
+    const marker = document.getElementById('marker');
+    const counterDispley = document.getElementById('counter');
+    const colorBtn = document.getElementById('changeColor');
+    // evento
+    marker.addEventListener('markerFound', () => {
+       counter ++;
+        counterDispley.textContent = counter;
+    })
+    //evento
+     marker.addEventListener('markerLost', () => {
+        console.log('marcdor se perdio')
+     });
+    // cambiar el color
+    colorBtn.addEventListener('click', () => {
+        const randomColor = colors[Math.floor(Math.random()*colors.length)]
+        arBox.setAttribute('material', 'color', randomColor);
+    });
+});
